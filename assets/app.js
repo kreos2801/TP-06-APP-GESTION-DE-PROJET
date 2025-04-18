1be4db96316d377911c4962e9a4e396b1cbe6a9d
gaouditz <gaouditz@et.esiea.fr>
2025-04-18 13:11:25 +0200

    fixing bug

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Utiliser le chemin relatif correct pour le JS de Bootstrap
 import 'bootstrap';
 import './styles/app.css';
