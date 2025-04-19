f7d21129329ac06fbfc1d3b71210781f4fa7799a
gaouditz <gaouditz@et.esiea.fr>
2025-04-19 05:22:10 +0200

    dynamic attachment delete add

--- src/Service/AttachmentService.php
+++ src/Service/AttachmentService.php
@@ -6,7 +6,6 @@ use App\Entity\Attachment;
 use App\Entity\Issue;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -19,8 +18,6 @@ class AttachmentService
     ){
     }
 
-
-
     public function handleUploadedAttachment(Issue $issue, Request $request): ?Attachment
     {
         /** @var ?UploadedFile $attachmentFile */
@@ -30,13 +27,15 @@ class AttachmentService
             return null;
         }
 
-        $filename =$this->uniqueFilename($attachmentFile);
+        $filename = $this->uniqueFilename($attachmentFile);
 
         $attachment = new Attachment();
         $attachment->setIssue($issue);
         $attachment->setOriginalName($attachmentFile->getClientOriginalName());
-        $attachment->setPath($this->absoluteAttachmentsDirectory . DIRECTORY_SEPARATOR . $filename);
+        // Stocker le chemin relatif dans la base de données
+        $attachment->setPath($this->absoluteAttachmentsDirectory . '/' . $filename);
 
+        // Déplacer le fichier physiquement
         $attachmentFile->move($this->attachmentsDirectory, $filename);
 
         $this->em->persist($attachment);
@@ -47,6 +46,21 @@ class AttachmentService
 
     private function uniqueFilename(UploadedFile $file): string
     {
-        return (uniqid(more_entropy: true). '.' .$file->guessExtension());
+        return uniqid(more_entropy: true) . '.' . $file->guessExtension();
+    }
+
+    public function delete(Attachment $attachment): void
+    {
+        // Extraire le nom du fichier de la propriété path
+        $dbPath = $attachment->getPath();
+        $filename = basename($dbPath);
+        
+        // Construire le chemin physique complet du fichier
+        $filepath = $this->attachmentsDirectory . '/' . $filename;
+        
+        // Vérifier que le fichier existe avant de le supprimer
+        if (file_exists($filepath)) {
+            unlink($filepath);
+        }
     }
 }
--- src/Twig/Components/Issue.php
+++ src/Twig/Components/Issue.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentToolsTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
@@ -25,9 +26,9 @@ class Issue
     #[LiveProp(writable: ['description', 'summary'])]
     public IssueEntity $issue;
 
-    /** @var Attachment[]   */
+    /** @var Attachment[] */
     #[LiveProp]
-    public array $attachment = [];
+    public array $attachments = [];
 
     #[LiveProp]
     public bool $isEditingSummary = false;
@@ -91,8 +92,39 @@ class Issue
     }
 
     #[LiveAction]
-    public function addAttachement(Request $request)
+    public function addAttachment(Request $request)
     {
-        $this->attachmentService->handleUploadedAttachment($this->issue, $request);
+        $attachment = $this->attachmentService->handleUploadedAttachment($this->issue, $request);
+        
+        if ($attachment) {
+            // Rafraîchir la liste des pièces jointes
+            $this->attachments = $this->issue->getAttachment()->toArray();
+        }
+    }
+    
+    #[LiveAction]
+    public function deleteAttachment(#[LiveArg] int $attachmentId): void
+    {
+        $attachment = $this->em->getRepository(Attachment::class)->find($attachmentId);
+        
+        if (!$attachment) {
+            return;
+        }
+        
+        // Vérifiez que l'attachment appartient bien à l'issue courante
+        if ($attachment->getIssue()->getId() !== $this->issue->getId()) {
+            return;
+        }
+        
+        // Enlever l'attachment de l'issue
+        $this->issue->removeAttachment($attachment);
+        
+        // Supprimer l'attachment de la base de données
+        // La suppression du fichier sera gérée par l'EntityListener
+        $this->em->remove($attachment);
+        $this->em->flush();
+        
+        // Mettre à jour la liste des attachments affichés
+        $this->attachments = $this->issue->getAttachment()->toArray();
     }
 }
